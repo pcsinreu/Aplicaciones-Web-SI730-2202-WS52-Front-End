182961069b8705f1b9c04e7dde7d26698ff2a7db
‘nreupomckinsey’ <naldo_reupo-musayon@mckinsey.com>
2022-09-06 23:00:07 -0500

    style(form) : Form

--- src/App.vue
+++ src/App.vue
@@ -2,54 +2,49 @@
 export default {
   data() {
     return {
-      cities: [
-        {name: 'New York', code: 'NY'},
-        {name: 'Rome', code: 'RM'},
-        {name: 'London', code: 'LDN'},
-        {name: 'Istanbul', code: 'IST'},
-        {name: 'Paris', code: 'PRS'}
-      ]
-    }
-  }
-}
+      value:"",
+      categories: [
+        { name: "Lácteos", code: "LA" },
+        { name: "Verduras", code: "VE" },
+        { name: "Frutas", code: "FR" },
+      ],
+    };
+  },
+};
 </script>
 
 <template>
   <form>
-
     <div class="card">
-
       <div class="field grid">
-        <label class="col-2 mb-2" for="code">Codigo*</label>
-        <div class="col-10">
-          <InputText id="code"/>
-          <Button label="validar"></Button>
+        <label class="col-3 mb-2" for="code">Codigo*</label>
+        <div class="col-7 p-0">
+          <InputText id="code" />
+        </div>
+        <div class="col-2">
+          <Button label="validar" />
         </div>
       </div>
       <div class="field grid">
-
-        <label for="nombre">Nombre*</label>
-        <InputText id="nombre"/>
-
+        <label for="nombre" class="col-3 mb-2" >Nombre*</label>
+        <div class="9"> <InputText id="nombre" class="w-full" /></div>
       </div>
-
       <div class="field grid">
-
-        <label for="Telefono">Telefono*</label>
-        <InputMask id="Telefono" v-model="value" mask="a9-99999a"/>
-        <div>
-        </div>
-
-        <label for="Telefono">Telefono*</label>
-        <pv-dropdown :options="cities" optionLabel="code" placeholder="Select a category"></pv-dropdown>
-
-
+        <label for="Telefono" class="col-3 mb-2">Telefono*</label>
+        <div class=9> <InputMask id="Telefono" v-model="value" mask="a9-99999a" class="w-full" /></div>
       </div>
-
+      <div class="field grid">
+        <label for="category" class="col-3 mb-2">Cetegoría*</label>
+        <div class="col-9 p-0"> <pv-dropdown
+          id="category"
+          :options="categories"
+          optionLabel="code"
+          placeholder="Select a category"
+          class="w-full"
+        ></pv-dropdown></div>
+      </div>
     </div>
   </form>
-
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
